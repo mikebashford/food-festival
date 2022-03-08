fd1d1df3a3530ad59619f72c57209d294fa2767e
Michael Bashford <mikebashford@gmail.com>
2022-03-08 14:58:11 -0600

    Added pwa to the app

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 const path = require("path");
 const webpack = require("webpack");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 module.exports = {
   entry: {
     app: "./assets/js/script.js",
     events: "./assets/js/events.js",
     schedule: "./assets/js/schedule.js",
-    tickets: "./assets/js/tickets.js"
+    tickets: "./assets/js/tickets.js",
   },
   output: {
     filename: "[name].bundle.js",
@@ -23,29 +24,46 @@ module.exports = {
             loader: "file-loader",
             options: {
               esModule: false,
-              name (file) {
-                return "[path][name].[ext]"
+              name(file) {
+                return "[path][name].[ext]";
               },
-              publicPath: function(url) {
-                return url.replace("../", "/assets/")
-              }
-            }  
+              publicPath: function (url) {
+                return url.replace("../", "/assets/");
+              },
+            },
           },
           {
-            loader: 'image-webpack-loader'
-          }
-        ]
-      }
-    ]
+            loader: "image-webpack-loader",
+          },
+        ],
+      },
+    ],
   },
-  plugins:[
+  plugins: [
     new webpack.ProvidePlugin({
       $: "jquery",
-      jQuery: "jquery"
+      jQuery: "jquery",
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: "static", // the report outputs to an HTML file in the dist folder
-    })
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [
+        {
+          src: path.resolve("assets/img/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
+    }),
   ],
-  mode: 'development'
+  mode: "development",
 };
